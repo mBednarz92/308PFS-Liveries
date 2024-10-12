0db59a0f7f18c39598816b318810269376b6c65a
mBednarz92 <115557975+mBednarz92@users.noreply.github.com>
2024-10-12 20:01:21 +0200

    Spitfire update for 2024 Q4, ZFX and ZFG added

---
+++ SpitfireLFMkIX/no. 308 PFS Loki ZF-L MJ888/description.lua
@@ -0,0 +1,63 @@
+livery = 
+{
+    {"SpitfireIX", 0, "308__PFS", false};
+    {"SpitfireIX", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	
+	{"NUMBER_R_0", 0, "308__PFS", false};
+	{"NUMBER_R_0", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_R_0", 3, "empty", false};    
+	{"NUMBER_R_00", 0, "308__PFS", false};
+	{"NUMBER_R_00", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_R_00", 3, "empty", false};    
+	{"NUMBER_R_000", 0, "308__PFS", false};
+	{"NUMBER_R_000", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_R_000", 3, "empty", false};
+    
+	{"NUMBER_L_0", 0, "308__PFS", false};
+	{"NUMBER_L_0", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_L_0", 3, "empty", false};	
+	{"NUMBER_L_00", 0, "308__PFS", false};
+	{"NUMBER_L_00", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_L_00", 3, "empty", false};    
+	{"NUMBER_L_000", 0, "308__PFS", false};
+	{"NUMBER_L_000", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_L_000", 3, "empty", false};
+    
+	{"NUMBER_L_F3_0", 0, "308__PFS", false};
+	{"NUMBER_L_F3_0", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_L_F3_0", 3, "empty", false};   
+	{"NUMBER_L_F3_00", 0, "308__PFS", false};
+	{"NUMBER_L_F3_00", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_L_F3_00", 3, "empty", false};   
+	{"NUMBER_L_F3_000", 0, "308__PFS", false};
+	{"NUMBER_L_F3_000", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_L_F3_000", 3, "empty", false};
+    
+	{"NUMBER_R_F3_000", 0, "308__PFS", false};
+	{"NUMBER_R_F3_000", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_R_F3_000", 3, "empty", false};   
+	{"NUMBER_R_F3_00", 0, "308__PFS", false};
+	{"NUMBER_R_F3_00", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_R_F3_00", 3, "empty", false};    
+	{"NUMBER_R_F3_0", 0, "308__PFS", false};
+	{"NUMBER_R_F3_0", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_R_F3_0", 3, "empty", false};  
+  
+	{"NUMBER_R_F3_NN", 0, "308__PFS", false};
+	{"NUMBER_R_F3_NN", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_R_F3_NN", 3, "empty", false};    
+	{"NUMBER_R_F3_N", 0, "308__PFS", false};
+	{"NUMBER_R_F3_N", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_R_F3_N", 3, "empty", false};
+    
+	{"NUMBER_L_F3_NN", 0, "308__PFS", false};
+	{"NUMBER_L_F3_NN", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_L_F3_NN", 3, "empty", false};    
+	{"NUMBER_L_F3_N", 0, "308__PFS", false};
+	{"NUMBER_L_F3_N", ROUGHNESS_METALLIC ,"TEST_SpitfireIX_DIF_RoughMet_GENERICPAINTED",false};
+	{"NUMBER_L_F3_N", 3, "empty", false};
+    
+	
+}
+name = "no. 308 PFS Loki ZF-L MJ888"
+countries = {"USA", "UK", "POL"}
